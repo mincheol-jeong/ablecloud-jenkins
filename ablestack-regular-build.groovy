d1e3028fe739f44653a02393ce6ae7fa6ad7b14a
mincheol-jeong <aksemr19@naver.com>
2022-12-08 10:49:44 +0900

    .

--- ablestack-regular-build.groovy
+++ ablestack-regular-build.groovy
@@ -39,90 +39,16 @@ pipeline {
            }
        }
 
-       stage('Cockpit Build') {
-           steps{
-
-               build 'cockpit'
-           }
-       }
-        
-        stage('Cockpit-plugin Build') {
-           steps{
-
-               build 'cockpit-plugin'
-           }
-       }
-
-        stage('Glue Build') {
-            steps{
-
-                build 'glue-build'
-            }
-       }
 
        stage('Glue RPM Copy to mirroring') {
            steps{
 
-               sh(""" 'rm -rf /root/mirror/data/centos/glue/*' """)
+               sh(""" rm -rf /root/mirror/data/centos/glue/* """)
                sh(""" cp -rf ${BRF}/*Glue*.rpm /root/mirror/data/centos/glue """)
-               sh(""" 'createrepo /root/mirror/data/centos/glue/.' """)
+               sh(""" createrepo /root/mirror/data/centos/glue/. """)
            }
        }
 
-       stage('Glue Image Build And DockerHub Push') {
-           steps{
-
-               build 'glue-image'
-           }
-       }
-
-        stage('Mold Build') {
-            steps{
-
-                build 'mold'
-            }
-        }
-
-        stage('Netdive Build') {
-            steps{
-
-                build 'netdive-ui'
-            }
-        }
-
-        stage('Wall Build') {
-            steps{
-
-                build 'wall-build'
-            }
-        }
-
-       stage('Build result file Move to version folder') {
-           steps{
-
-                  sh("""cp -r ${BRF}/* ${JWF}/${NEW_DATE}/""")
-           }
-       }
-
-        stage('Ablestack Template Create') {
-            steps{
-
-                build 'make-qcow2-template'
-            }
-        }
-        
-        stage('Ablestack ISO Create') {
-            steps{
-
-                build 'ablestack-kickstart'
-            }
-        }
-        
-        stage('Ablestack ISO Copy') {
-            steps{
-                sh("""cp -r ${JWF}/ISO/* ${JWF}/${NEW_DATE}/""")
-                sh("""rm -rf ${JWF}/ISO/*""")
-            }
+     
         }
     }
-}
