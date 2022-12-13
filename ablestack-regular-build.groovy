1b0c50e08979b0ff46ca24006579e3049867bae9
mincheol-jeong <aksemr19@naver.com>
2022-12-13 09:38:59 +0900

    .

--- ablestack-regular-build.groovy
+++ ablestack-regular-build.groovy
@@ -24,7 +24,7 @@ pipeline {
                    int intC = c as int
                    intC = intC +1
                    println(intC)
-                   def dateFormat = new SimpleDateFormat('MMdd')
+                   def dateFormat = new SimpleDateFormat('yyMMdd')
                    def today = new Date()
                    def newDate = dateFormat.format(today)
                    def newVersionInfo = codeName + '-' + a + '.' + b + '.' + intC.toString() + '-' + newDate
@@ -39,90 +39,7 @@ pipeline {
            }
        }
 
-       stage('Cockpit Build') {
-           steps{
 
-               build 'cockpit'
-           }
-       }
         
-        stage('Cockpit-plugin Build') {
-           steps{
-
-               build 'cockpit-plugin'
-           }
-       }
-
-        stage('Glue Build') {
-            steps{
-
-                build 'glue-build'
-            }
-       }
-
-       stage('Glue RPM Copy to mirroring') {
-           steps{
-
-               sh(""" rm -rf /root/mirror/data/centos/glue/* """)
-               sh(""" cp -rf ${BRF}/*Glue*.rpm /root/mirror/data/centos/glue """)
-               sh(""" createrepo /root/mirror/data/centos/glue/. """)
-           }
-       }
-
-       stage('Glue Image Build And DockerHub Push') {
-           steps{
-
-               build 'glue-image'
-           }
-       }
-
-        stage('Mold Build') {
-            steps{
-
-                build 'mold'
-            }
-        }
-
-        stage('Netdive Build') {
-            steps{
-
-                build 'netdive-ui'
-            }
-        }
-
-        stage('Wall Build') {
-            steps{
-
-                build 'wall-build'
-            }
-        }
-
-       stage('Build result file Move to version folder') {
-           steps{
-
-                  sh("""cp -r ${BRF}/* ${JWF}/${NEW_DATE}/""")
-           }
-       }
-
-        stage('Ablestack Template Create') {
-            steps{
-
-                build 'make-qcow2-template'
-            }
-        }
-        
-        stage('Ablestack ISO Create') {
-            steps{
-
-                build 'ablestack-kickstart'
-            }
-        }
-        
-        stage('Ablestack ISO Copy') {
-            steps{
-                sh("""cp -r ${JWF}/ISO/* ${JWF}/${NEW_DATE}/""")
-                sh("""rm -rf ${JWF}/ISO/*""")
-            }
-        }
     }
 }
